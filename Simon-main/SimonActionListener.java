de6d8b94605beffcdca7c83d8346e597062dbeb2
Deler <del2547.pv@gmail.com>
2024-02-08 23:43:03 +0700

    add SimonActionListener.java file and update Simongame.java

---
+++ Simon-main/SimonActionListener.java
@@ -0,0 +1,20 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+
+/**
+ * SimonActionListener
+ */
+public class SimonActionListener implements ActionListener{
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        JButton input = (JButton)e.getSource();
+        
+        // TODO Auto-generated method stub
+        // throw new UnsupportedOperationException("Unimplemented method 'actionPerformed'");
+    }
+
+    
+}
--- Simon-main/Simongame.java
+++ Simon-main/Simongame.java
@@ -1,10 +1,15 @@
 // import javax.swing.*;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
+import java.util.Random;
 class Simongame {
 
     JFrame frame; // window frame
@@ -13,25 +18,68 @@ class Simongame {
     JLabel textHighscore, numHighscore; // label"Highscore:" and Labelhighscore
     JButton btnQuit; // quit button
     JButton lPad, rPad, upPad, downPad; // leftpad rightpad uppad downpad
+    Icon left, right, up, down;
+    int countgen = 1; // count for generate pattern (level will increase from countgen)
     
     Simongame(){
         frame = new JFrame();
         frame.setSize(450, 400);
-        gameDetails();
         frame.setVisible(true);
         frame.setLayout(new FlowLayout());
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        gameDetails();
         // frame.setLayout(null);
     }
     void gameDetails(){
         btnPlay = new JButton("Play");
-        // btnPlay.setPreferredSize(new Dimension(100, 50));
-        btnPlay.setBounds(250, 350, 100, 50);
+        btnPlay.setPreferredSize(new Dimension(100, 50));
+        // btnPlay.setBounds(250, 350, 100, 50);
         btnQuit = new JButton("Quit");
         btnQuit.setPreferredSize(new Dimension(100, 50));
 
+        textScore = new JLabel("score: ");
+        numHighscore = new JLabel("0");
+
+        lPad = new JButton("left");
+        rPad = new JButton("right");
+        upPad = new JButton("up");
+        downPad = new JButton("down");
+
         frame.add(btnPlay);
         frame.add(btnQuit);
+    }
+    void inGamePhase(){
+        try {
+            left = new ImageIcon();
+            right = new ImageIcon();
+            up = new ImageIcon();
+            down = new ImageIcon();
+        } catch (Exception e) {
+            // TODO: handle exception
+            System.err.println(e);
+        }
+        lPad.setIcon(left);
+        rPad.setIcon(right);
+        upPad.setIcon(up);
+        downPad.setIcon(down);
 
+        SimonActionListener action = new SimonActionListener();
+
+    }
+    void genPattern(){
+        Random rand = new Random();
+        int arr [] = new int[4];
+        if (countgen < 4){
+            int a = rand.nextInt(4);
+            
+        }
+        for (int i = 0; i < arr.length; i++){
+            // random amount tick in one pad 
+            // random 4 pad pattern
+            arr[i] = Math.random() > 0.49 ? 1 : 0;
+            
+            
+        }
     }
     // public static void main(String[] args) {
     //     Simongame game = new Simongame();
